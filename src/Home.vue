b52bb0167c27bfc6305aa176604c9be724eaf5c7
teriyakiegg <teriyakiegg@gmail.com>
2020-06-24 22:36:13 +0900

    Home画面にCounter追加

--- src/Home.vue
+++ src/Home.vue
@@ -1,7 +1,32 @@
 <template>
   <v-ons-page>
     <p style="text-align: center">
-      Welcome home.
+      Counter
     </p>
+    <div id="vue-counter">
+  <h3>Counter: {{ counter }}</h3>
+    <button type="button" v-on:click="increase">Increase By 1</button> <button type="button" v-on:click="decrease">Decrease By 1</button> <button type="button" v-on:click="reset">Reset Counter</button>
+  </div>
   </v-ons-page>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increase: function() {
+      this.counter++;
+    },
+    decrease:  function() {
+      this.counter--;
+    },
+    reset: function() {
+      this.counter = 0;
+    }
+  },
+};
+</script>
